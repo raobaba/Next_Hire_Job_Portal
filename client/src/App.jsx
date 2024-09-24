5472cb852c83d45e5716162821f2387b390879a9
Rajan Kumar <raorajan9576@gmail.com>
2024-09-24 07:59:43 +0530

    Browse component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Jobs from "./components/Jobs";
 import JobDescription from "./components/JobDescription";
+import Browse from "./components/Browse";
 
 const appRouter = createBrowserRouter([
   {
@@ -11,20 +12,24 @@ const appRouter = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: '/signup',
-    element: <Signup />
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    path:'/jobs',
-    element:<Jobs/>
+    path: "/jobs",
+    element: <Jobs />,
   },
   {
     path: "/description/:id",
-    element: <JobDescription />
+    element: <JobDescription />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
   },
 ]);
 
