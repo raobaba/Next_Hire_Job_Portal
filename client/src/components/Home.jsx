c9a9a6da054d621ca1022b12e488053ac4ddc38b
Rajan Kumar <raorajan9576@gmail.com>
2024-09-24 06:13:25 +0530

    Category Carousel

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "@/components/shared/Navbar";
 import HeroSection from "./HeroSection";
 import Footer from "@/components/shared/Footer";
+import CategoryCarousel from "./CategoryCarousel";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection/>
+      <CategoryCarousel/>
       <Footer />
     </div>
   );
